ec6b4b35359de23888cc51cbae2410b9adc83479
vudaodev <v24dao@gmail.com>
2026-01-29 13:58:27 +0000

    Restrict our game to 60 FPS using 'pygame.time.Clock()' and dt (time delta). CH2/L4

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init()
-    # new instance of GUI:
+    # Initialise pygame
+    pygame.init() 
+
+    # Variables to keep track of time
+    clock = pygame.time.Clock()
+    dt = 0 # delta time (seconds). Time that has passed since the last frame was drawn.
+
+    # New instance of GUI
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Game Loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,9 +25,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip(
-        
-        )
+        pygame.display.flip()
+
+        # Pause game until 1/60th of a second has passed, and update dt
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
