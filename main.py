f6ba8babee1016eb45874445d541a8abee094d1d
vudaodev <v24dao@gmail.com>
2026-01-29 14:49:54 +0000

    Instantiate a Player Object and draw player on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,6 +18,9 @@ def main():
     # New instance of GUI
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Instantiate a Player Object
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     # Game Loop
     while True:
         log_state()
@@ -25,10 +29,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        #Re-render player
+        player.draw(screen)
+
         pygame.display.flip()
 
         # Pause game until 1/60th of a second has passed, and update dt
         dt = clock.tick(60)/1000
+
+        
 
 if __name__ == "__main__":
     main()
