fe97158906c1feda97fc4be09a134589835c7dc1
vudaodev <v24dao@gmail.com>
2026-01-29 13:38:22 +0000

    Add Game Loop. CH2/L3

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    # new instance of GUI:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            #Check if user has closed the window. Exit the game loop if they do.
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip(
+        
+        )
+
+if __name__ == "__main__":
+    main()
