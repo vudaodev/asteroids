71f3b19c415c2d58334b4bea4b3cb2624834431b
vudaodev <v24dao@gmail.com>
2026-02-02 13:10:26 +0000

    Add logic to make bullets spawn at the tip of Player object

--- player.py
+++ player.py
@@ -44,8 +44,10 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        new_shot = Shot(self.position[0], self.position[1]) #Create a new Shot at the current position of player
+    
         unit_vector = pygame.Vector2(0,1)
         rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED 
+        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+
+        new_shot = Shot(self.position.x + rotated_vector.x * self.radius, self.position.y + rotated_vector.y * self.radius ) #Create a new Shot at the current position of player 
         new_shot.velocity - rotated_with_speed_vector
