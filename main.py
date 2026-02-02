302d0e509b892c153029144c6ec1f2357e186be2
vudaodev <v24dao@gmail.com>
2026-02-02 15:12:30 +0000

    Add asteroid split logic to main game

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()        
+                    asteroid.split()        
 
             
         # Re-render drawable sprites
